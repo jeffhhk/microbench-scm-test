f4e6162e9410fc46bf9b7356032b7ea4fc1b0ad9
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2023-08-03 19:51:32 -0700

    run-on-gambit.sh: use unsafe; use make-u8vector

--- run-on-gambit.sh
+++ run-on-gambit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 adirProj=$( cd $( dirname "$0" ) && pwd )
 set -euo pipefail
-gsc -exe -nopreload \
+gsc -prelude '(declare (standard-bindings) (block) (not safe))(define-macro (make-bytevector n) `(make-u8vector ,n))(define-macro (bytevector-u8-ref v i) `(u8vector-ref ,v ,i))(define-macro (bytevector-u8-set! v i x) `(u8vector-set! ,v ,i ,x))' \
+    -exe -nopreload \
     microbench/ \
     microbench/microbench/microbench.sld \
     app5_gambit/app5_gambit.sld
